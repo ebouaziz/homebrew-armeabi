5b7851e9557222420ff3d2b2ff83b6d6bc9f2c56
Emmanuel Bouaziz <ebouaziz@neotion.com>
2015-01-23 17:26:06 +0100

    add linux specific build option

--- ppl11.rb
+++ ppl11.rb
@@ -5,16 +5,25 @@ class Ppl11 < Formula
   url 'http://bugseng.com/products/ppl/download/ftp/releases/1.1/ppl-1.1.tar.gz'
   sha1 'd24c79f7299320e6a344711aaec74bd2d5015b15'
 
-  depends_on 'homebrew/dupes/m4' => :build if MacOS.version < :leopard
+  if OS.linux?
+  else
+    depends_on 'homebrew/dupes/m4' => :build if MacOS.version < :leopard
+  end
   depends_on 'gmp'
 
   def install
-    args = [
-      "--prefix=#{prefix}",
-      "--disable-dependency-tracking",
-      "--with-gmp-prefix=#{Formula.factory('gmp').opt_prefix}"
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --with-gmp=#{Formula.factory('gmp').opt_prefix}
     ]
-
+    if OS.linux?
+        args << "--disable-shared"
+        args << "--host=core2-unknown-linux-gnu"
+        args << "--build=core2-unknown-linux-gnu"
+        args << "--disable-ppl_lpsol"
+        args << "--disable-ppl_lcdd"
+    end
     system "./configure", *args
     system "make install"
   end
